35089bc444b1bf605564cf1ee959e956d2875d3d
Sheepinav <abhaskar@berkeley.edu>
2020-07-27 00:03:33 -0500

    removing all files not necessary for gh pages

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Textbox from "./Textbox.js";
-
-const wrapper = document.getElementById("root");
-ReactDOM.render(
-    <div>
-        <Textbox />
-    </div>, wrapper)
